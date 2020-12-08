cc1e9d7782aff601ce137ed90b10c03f7a5be51f
Andrey <freeman49@inbox.ru>
2020-12-08 16:22:20 +0800

    second lesson

---
+++ js/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// if  (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('OK');
+// }
+
+// (num === 50) ? console.log('Ok') : console.log('Error');
+
+const num = 50;
+
+// switch (num) {
+//     case 49:
+//         console.log('НЕверно');
+//         break;
+//     case 100:
+//         console.log('НЕверно');
+//         break;
+//     case 50:
+//         console.log('верно');
+//         break;
+//     default:
+//         console.log('Не в этот раз');
+//         break;
+// }
+
